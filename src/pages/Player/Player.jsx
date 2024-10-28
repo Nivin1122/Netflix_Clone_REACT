938265a30f6db6fd7699e934636d5f77fb3f5f7c
nivin <bnivin71@gmail.com>
2024-10-28 21:46:44 +0530

    some changes were added

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 const Player = () => {
   return (
-    <div>Player</div>
+    <div className='player'>
+      <img src={back_arrow_icon}></img>
+      <iframe width="90%" height="90%" src="https://www.youtube.com/embed/hkHHwA-vEyQ" title='trailer' frameBorder="0" allowFullScreen></iframe>
+      <div className="player-info">
+        <p>Published Date</p>
+        <p>Name</p>
+        <p>Type</p>
+      </div>
+    </div>
   )
 }
 
-export default Player
+export default Player 
